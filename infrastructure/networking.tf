b33cf02bdf3aaf255e70a9e85f68838783956caf
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-08-29 11:18:57 +0100

    Comment out private networking for Purview, to allow self serve analytics to work

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -1,3 +1,12 @@
+# Tooling vnet resources
+data "azurerm_virtual_network" "tooling" {
+  name                = local.tooling_config.network_name
+  resource_group_name = local.tooling_config.network_rg
+
+  provider = azurerm.tooling
+}
+
+# Create VNet
 resource "azurerm_virtual_network" "purview_resources_vnet" {
   name                = "vnet-pview"
   location            = local.location
@@ -33,6 +42,7 @@ resource "azurerm_subnet_network_security_group_association" "nsg" {
   ]
 }
 
+# VNet links
 resource "azurerm_private_dns_zone_virtual_network_link" "purview_resources_vnet_link" {
   for_each              = toset([for dns_zone in azurerm_private_dns_zone.data_lake_dns_zone : dns_zone.name])
   name                  = "dfs-vnet-pview"
@@ -42,6 +52,10 @@ resource "azurerm_private_dns_zone_virtual_network_link" "purview_resources_vnet
 
   tags = local.tags
 }
+
+/**
+# Commented out because private networking with Purview currently doesn't work with Unified Catalog solution integrations
+# https://learn.microsoft.com/en-us/purview/unified-catalog-self-serve-analytics
 
 resource "azurerm_private_dns_zone_virtual_network_link" "purview_vnet_link" {
   name                  = "dfs-vnet-pview-platform"
@@ -69,16 +83,10 @@ resource "azurerm_private_dns_zone_virtual_network_link" "purview_servicebus_vne
 
   tags = local.tags
 }
+**/
 
 
-# Tooling vnet resources
-data "azurerm_virtual_network" "tooling" {
-  name                = local.tooling_config.network_name
-  resource_group_name = local.tooling_config.network_rg
-
-  provider = azurerm.tooling
-}
-
+# Network peering
 resource "azurerm_virtual_network_peering" "purview_to_tooling" {
   name                      = "pins-peer-pview-to-tooling-${var.environment}"
   resource_group_name       = azurerm_resource_group.data_management.name
@@ -103,6 +111,9 @@ resource "azurerm_private_dns_zone" "data_lake_dns_zone" {
 
   tags = local.tags
 }
+/**
+# Commented out because private networking with Purview currently doesn't work with Unified Catalog solution integrations
+# https://learn.microsoft.com/en-us/purview/unified-catalog-self-serve-analytics
 
 resource "azurerm_private_dns_zone" "purview_dns_zone" {
   # Purview account
@@ -126,6 +137,7 @@ resource "azurerm_private_dns_zone" "service_bus_dns_zone" {
 
   tags = local.tags
 }
+**/
 
 data "azurerm_private_dns_zone" "tooling_storage" {
   for_each = toset(local.storage_zones)
@@ -136,29 +148,7 @@ data "azurerm_private_dns_zone" "tooling_storage" {
   provider = azurerm.tooling
 }
 
-resource "azurerm_private_endpoint" "purview_platform_private_endpoint" {
-  name                = "pins-pe-platform${azurerm_purview_account.management.name}"
-  resource_group_name = azurerm_resource_group.data_management.name
-  location            = local.location
-  subnet_id           = azurerm_subnet.purview_resources_subnet.id
-
-  private_dns_zone_group {
-    name = "purviewDnsZone"
-    private_dns_zone_ids = concat(
-      [for dns_zone in azurerm_private_dns_zone.data_lake_dns_zone : dns_zone.id],
-      [azurerm_private_dns_zone.purview_dns_zone.id, azurerm_private_dns_zone.purview_platform_dns_zone.id]
-    )
-  }
-
-  private_service_connection {
-    name                           = "purviewDfs"
-    is_manual_connection           = false
-    private_connection_resource_id = azurerm_purview_account.management.id
-    subresource_names              = ["platform"]
-  }
-
-  tags = local.tags
-}
+# Private endpoints
 
 resource "azurerm_private_endpoint" "data_lake" {
   name                = "pins-pe-${azurerm_storage_account.dedicated_purview_storage.name}"
@@ -205,7 +195,33 @@ resource "azurerm_private_endpoint" "tooling_data_lake" {
 
   tags = local.tags
 }
+/**
+# Commented out because private networking with Purview currently doesn't work with Unified Catalog solution integrations
+# https://learn.microsoft.com/en-us/purview/unified-catalog-self-serve-analytics
 
+resource "azurerm_private_endpoint" "purview_platform_private_endpoint" {
+  name                = "pins-pe-platform${azurerm_purview_account.management.name}"
+  resource_group_name = azurerm_resource_group.data_management.name
+  location            = local.location
+  subnet_id           = azurerm_subnet.purview_resources_subnet.id
+
+  private_dns_zone_group {
+    name = "purviewDnsZone"
+    private_dns_zone_ids = concat(
+      [for dns_zone in azurerm_private_dns_zone.data_lake_dns_zone : dns_zone.id],
+      [azurerm_private_dns_zone.purview_dns_zone.id, azurerm_private_dns_zone.purview_platform_dns_zone.id]
+    )
+  }
+
+  private_service_connection {
+    name                           = "purviewDfs"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_purview_account.management.id
+    subresource_names              = ["platform"]
+  }
+
+  tags = local.tags
+}
 
 # Purview managed private endpoints
 resource "azurerm_private_endpoint" "purview_ingestion_blob" {
@@ -261,3 +277,4 @@ resource "azurerm_private_endpoint" "purview_ingestion_namespace" {
     subresource_names              = ["namespace"]
   }
 }
+**/
