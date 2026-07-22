63d541abdb701fa6fdc8ebe15e6d3934e84a33c3
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-07-22 09:42:43 +0100

    chore: adding tags to prod resources & removing tooling references

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -4,11 +4,6 @@ locals {
   data_lake_retention_days = 28
   #vnet_base_cidr_block     = "10.111.0.0/24"
   #vnet_subnet_ip_range     = "10.111.0.0/25"
-  tooling_config = {
-    network_name    = "pins-vnet-shared-tooling-uks"
-    network_rg      = "pins-rg-shared-tooling-uks"
-    subscription_id = "edb1ff78-90da-4901-a497-7e79f966f8e2"
-  }
   #storage_zones = ["blob", "dfs", "file", "queue", "table", "web"]  # Commented out because the networking currently cannot be used
   dedicated_purview_storage_role_assignments = [
     {
@@ -16,9 +11,20 @@ locals {
       principal_id         = "0cad1989-27de-4242-a06b-7cad373497e7" # Azure DevOps Pipelines - ODW Prod - Infrastructure"
     }
   ]
-  tags = {
-    CreatedBy   = "Terraform"
-    Environment = var.environment
-    ServiceName = local.service_name
-  }
+
+  tags = merge(
+    {
+      CreatedBy   = "Terraform"
+      Environment = var.environment
+      ServiceName = local.service_name
+    },
+    var.environment == "prod" ? {
+      SystemAssetOwner    = var.system_asset_owner
+      BusinessProcess     = "ODW"
+      PersonalData        = "No"
+      SpecialCategoryData = "No"
+      ProtectiveMarking   = "Official-Sensitive-Mission-Critical"
+      CriticalityRating   = "Level 2"
+    } : {}
+  )
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,9 @@ variable "environment" {
   description = "The name of the environment in which resources will be deployed"
   type        = string
 }
+
+variable "system_asset_owner" {
+  description = "tagging - value extracted from ADO library secret"
+  type        = string
+  sensitive   = true
+}
